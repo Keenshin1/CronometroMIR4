a57366c99b4d1967a751ab39b248f3691916ce11
Nielsen Gonçalves <nielsenviper@hotmail.com>
2021-11-09 11:24:26 -0300

    Teste

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace CronometroMIR4
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.BtnIniciar = new System.Windows.Forms.Button();
             this.BtnParar = new System.Windows.Forms.Button();
@@ -37,6 +38,10 @@ namespace CronometroMIR4
             this.RtxbComentario = new System.Windows.Forms.RichTextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.RButtonXP = new System.Windows.Forms.RadioButton();
+            this.RButtonMineirando = new System.Windows.Forms.RadioButton();
+            this.RButtonRecolhendo = new System.Windows.Forms.RadioButton();
+            this.RButtonMeditando = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // label1
@@ -52,7 +57,7 @@ namespace CronometroMIR4
             // 
             // BtnIniciar
             // 
-            this.BtnIniciar.BackColor = System.Drawing.Color.DarkGray;
+            this.BtnIniciar.BackColor = System.Drawing.Color.Silver;
             this.BtnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.BtnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnIniciar.Location = new System.Drawing.Point(12, 120);
@@ -65,7 +70,7 @@ namespace CronometroMIR4
             // 
             // BtnParar
             // 
-            this.BtnParar.BackColor = System.Drawing.Color.DarkGray;
+            this.BtnParar.BackColor = System.Drawing.Color.Silver;
             this.BtnParar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.BtnParar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnParar.Location = new System.Drawing.Point(310, 120);
@@ -78,10 +83,10 @@ namespace CronometroMIR4
             // 
             // BtnResetar
             // 
-            this.BtnResetar.BackColor = System.Drawing.Color.DarkGray;
+            this.BtnResetar.BackColor = System.Drawing.Color.Silver;
             this.BtnResetar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.BtnResetar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnResetar.Location = new System.Drawing.Point(12, 369);
+            this.BtnResetar.Location = new System.Drawing.Point(12, 419);
             this.BtnResetar.Name = "BtnResetar";
             this.BtnResetar.Size = new System.Drawing.Size(425, 40);
             this.BtnResetar.TabIndex = 3;
@@ -92,7 +97,7 @@ namespace CronometroMIR4
             // RtxbComentario
             // 
             this.RtxbComentario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RtxbComentario.Location = new System.Drawing.Point(12, 203);
+            this.RtxbComentario.Location = new System.Drawing.Point(12, 253);
             this.RtxbComentario.Name = "RtxbComentario";
             this.RtxbComentario.Size = new System.Drawing.Size(425, 160);
             this.RtxbComentario.TabIndex = 4;
@@ -103,7 +108,7 @@ namespace CronometroMIR4
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(8, 180);
+            this.label2.Location = new System.Drawing.Point(8, 230);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 20);
             this.label2.TabIndex = 5;
@@ -114,12 +119,68 @@ namespace CronometroMIR4
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // RButtonXP
+            // 
+            this.RButtonXP.AutoSize = true;
+            this.RButtonXP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RButtonXP.ForeColor = System.Drawing.Color.White;
+            this.RButtonXP.Location = new System.Drawing.Point(172, 120);
+            this.RButtonXP.Name = "RButtonXP";
+            this.RButtonXP.Size = new System.Drawing.Size(98, 24);
+            this.RButtonXP.TabIndex = 6;
+            this.RButtonXP.TabStop = true;
+            this.RButtonXP.Text = "Caçando";
+            this.RButtonXP.UseVisualStyleBackColor = true;
+            // 
+            // RButtonMineirando
+            // 
+            this.RButtonMineirando.AutoSize = true;
+            this.RButtonMineirando.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RButtonMineirando.ForeColor = System.Drawing.Color.White;
+            this.RButtonMineirando.Location = new System.Drawing.Point(172, 153);
+            this.RButtonMineirando.Name = "RButtonMineirando";
+            this.RButtonMineirando.Size = new System.Drawing.Size(115, 24);
+            this.RButtonMineirando.TabIndex = 7;
+            this.RButtonMineirando.TabStop = true;
+            this.RButtonMineirando.Text = "Mineirando";
+            this.RButtonMineirando.UseVisualStyleBackColor = true;
+            // 
+            // RButtonRecolhendo
+            // 
+            this.RButtonRecolhendo.AutoSize = true;
+            this.RButtonRecolhendo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RButtonRecolhendo.ForeColor = System.Drawing.Color.White;
+            this.RButtonRecolhendo.Location = new System.Drawing.Point(172, 183);
+            this.RButtonRecolhendo.Name = "RButtonRecolhendo";
+            this.RButtonRecolhendo.Size = new System.Drawing.Size(123, 24);
+            this.RButtonRecolhendo.TabIndex = 8;
+            this.RButtonRecolhendo.TabStop = true;
+            this.RButtonRecolhendo.Text = "Recolhendo";
+            this.RButtonRecolhendo.UseVisualStyleBackColor = true;
+            // 
+            // RButtonMeditando
+            // 
+            this.RButtonMeditando.AutoSize = true;
+            this.RButtonMeditando.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RButtonMeditando.ForeColor = System.Drawing.Color.White;
+            this.RButtonMeditando.Location = new System.Drawing.Point(172, 213);
+            this.RButtonMeditando.Name = "RButtonMeditando";
+            this.RButtonMeditando.Size = new System.Drawing.Size(111, 24);
+            this.RButtonMeditando.TabIndex = 9;
+            this.RButtonMeditando.TabStop = true;
+            this.RButtonMeditando.Text = "Meditando";
+            this.RButtonMeditando.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
-            this.ClientSize = new System.Drawing.Size(448, 417);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(52)))), ((int)(((byte)(83)))));
+            this.ClientSize = new System.Drawing.Size(448, 471);
+            this.Controls.Add(this.RButtonMeditando);
+            this.Controls.Add(this.RButtonRecolhendo);
+            this.Controls.Add(this.RButtonMineirando);
+            this.Controls.Add(this.RButtonXP);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.RtxbComentario);
             this.Controls.Add(this.BtnResetar);
@@ -127,11 +188,12 @@ namespace CronometroMIR4
             this.Controls.Add(this.BtnIniciar);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Cronometro MIR4 - V1.0";
+            this.Text = "Cronometro MIR4 - V1.2";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -147,6 +209,10 @@ namespace CronometroMIR4
         private System.Windows.Forms.RichTextBox RtxbComentario;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton RButtonXP;
+        private System.Windows.Forms.RadioButton RButtonMineirando;
+        private System.Windows.Forms.RadioButton RButtonRecolhendo;
+        private System.Windows.Forms.RadioButton RButtonMeditando;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Diagnostics;
-using System.IO;
+using System.Windows.Forms;
 
 namespace CronometroMIR4
 {
@@ -19,66 +11,115 @@ namespace CronometroMIR4
         {
             InitializeComponent();
         }
+        /*
+        HISTORICO DE ATUALIZAÇÕES
 
-        //
+        (V 1.1)
+            ADD BOTÕES DE ESCOLHA PARA CAÇA, MINERIO, MEDITAR E RECOLHER
+
+        (V 1.2)
+            Mudança no visual do app
 
         
+
+
+
+
+
+
+        */
+
         string DataInicial { get; set; }
 
         public void BtnIniciar_Click(object sender, EventArgs e)
         {
             // Botão Iniciar Cronometro
 
-
+            // Consulta a data atual e salva ela na variavel "DataInicial"
             DateTime DataInicial = DateTime.Now;
 
+            // Limpa o texto do RichTextBox Comentario
             RtxbComentario.Text = "";
 
+            // Inicia o cronometro
             stopWath.Start();
         }
+
+
 
         public void BtnParar_Click(object sender, EventArgs e)
         {
             // Botão Parar Cronometro
+
+            // Para o cronometro
             stopWath.Stop();
         }
+
+
 
         public void BtnResetar_Click(object sender, EventArgs e)
         {
             // Botão Resetar / Salvar Cronometro
 
-            
 
-            DateTime DataFinal = DateTime.Now;
+            // VERIFICAÇÃO DOS BOTÕES RADIAIS
+            string BotaoRadial = "Nenhum botão selecionado";
 
-
-            string TudoJunto = 
-                $"Data Inicial: {DataInicial}\n" +
-                $"Tempo Decorrido: {label1.Text}\n" +
-                $"Data Final: {DataFinal}\n" +
-                $"Comentario: {RtxbComentario.Text}\n";
-
-            using (System.IO.StreamWriter file =
-                    
-                    new System.IO.StreamWriter(@"MIR4.txt", true))
+            if (RButtonXP.Checked == true)
             {
-                file.WriteLine(TudoJunto);
+                BotaoRadial = "Caçando";
+            }
+            if (RButtonMineirando.Checked == true)
+            {
+                BotaoRadial = "Mineirando";
+            }
+            if (RButtonRecolhendo.Checked == true)
+            {
+                BotaoRadial = "Recolhendo Ervas";
+            }
+            if (RButtonMeditando.Checked == true)
+            {
+                BotaoRadial = "Meditando";
             }
 
 
+            // Consulta a data atual e salva ela na variavel "DataFinal"
+            DateTime DataFinal = DateTime.Now;
 
+            // Define o texto que vai ser escrito no txt e a ordem dele
+            string TudoJunto =
+                $"Data Inicial: {DataInicial}\n" +
+                $"Tempo Decorrido: {label1.Text}\n" +
+                $"Data Final: {DataFinal}\n" +
+                $"Ação executada: {BotaoRadial}\n" +
+                $"Comentario: {RtxbComentario.Text}\n";
 
+            // Exporta os dados para o arquivo txt
+            using (System.IO.StreamWriter file =
 
+                    // Define o caminho para salvar o arquivo txt
+                    new System.IO.StreamWriter(@"MIR4.txt", true))
+            {
+                // Escreve os dados no arquivo
+                file.WriteLine(TudoJunto);
+            }
+
+            // Para o cronometro
             stopWath.Reset();
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
             stopWath = new Stopwatch();
         }
 
+
+
         private void timer1_Tick(object sender, EventArgs e)
         {
+            // Define o formato da hora que vai aparecer no cronometro 
             this.label1.Text = string.Format("{0:hh\\:mm\\:ss}", stopWath.Elapsed);
         }
     }
